1fa8a18be5f015bf0943cb680c4664d89812e689
l3miage-dumaslo <loic.dumas@etu.univ-grenoble-alpes.fr>
2024-03-14 10:42:24 +0100

    TP1ex2

--- server/src/main/java/fr/uga/l3miage/tp1/exo2/models/NetflixUserEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo2/models/NetflixUserEntity.java
@@ -1,5 +1,18 @@
 package fr.uga.l3miage.tp1.exo2.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="netflix_user")
 public class NetflixUserEntity {
 
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "uuid")
+    private String uuid;
 }
